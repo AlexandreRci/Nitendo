38047ec35e28f3c026e3bec6279777d1ee120e0d
JudyPolka <polka.judythkael@gmail.com>
2025-03-21 11:24:25 +0100

    Modif test Modif classe Client

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,7 +1,14 @@
 package nintendo.test;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import nintendo.model.Achat;
 import nintendo.model.Adresse;
 import nintendo.model.Boutique;
+import nintendo.model.Client;
 import nintendo.model.Console;
 import nintendo.model.Hybride;
 import nintendo.model.Jeu;
@@ -21,11 +28,23 @@ public class Test {
 		
 		Boutique bt = new Boutique("Micromania", a1);
 		
+		
 		Jeu marioKartDs = new Jeu("Mario Kart", ds, bt);
 		Jeu marioKartWii = new Jeu("Mario Kart", wii, bt);
 		Jeu marioBros = new Jeu("Mario Bros", ds, bt);
 		Jeu farCry = new Jeu("Far Cry 4", play4, bt);
 		Jeu eldenRing = new Jeu("Elden Ring", pc, bt);
+
+		List<Achat> listeAchat1 = new ArrayList();
+		List<Achat> listeAchat2 = new ArrayList();
+		
+		Achat achat1 = new Achat(farCry, LocalDate.parse("2019-05-05"), "20");
+		Achat achat2 = new Achat(eldenRing, LocalDate.parse("2019-03-05"), "20");
+		Collections.addAll(listeAchat1,achat1,achat2);
+		listeAchat2.add(achat2);
+		
+		Client c1 = new Client("Abid","Jordan",listeAchat1);
+		Client c2 = new Client("Polka","Judy",listeAchat2);
 
 	}
 
